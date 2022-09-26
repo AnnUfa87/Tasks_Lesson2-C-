2e83cb58fb1fc7af42391a9d67315a2b4d8f30ba
Иванова Анна <babankak174@mail.ru>
2022-09-26 10:27:17 +0500

    Задача 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(number);
+Console.WriteLine($"вторая цифра данного числа равна: " + str[1]);
